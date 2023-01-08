402f720a01dbe494661adecabbf7adbfdb46a891
V0odo0 <Voodoo2211@gmail.com>
2023-01-08 20:22:48 +0500

    ST

--- Assets/Scripts/Core/GameUIManager.cs
+++ Assets/Scripts/Core/GameUIManager.cs
@@ -9,5 +9,28 @@ namespace HSH.UI
     {
         public UIPlayerHud PlayerHud => _playerHud;
         [SerializeField] private UIPlayerHud _playerHud;
+
+        [SerializeField] private UIPopUpPanel _isThatItPopUpPanel;
+        [SerializeField] private UIButton _isThatItButton;
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _isThatItButton.Button.onClick.AddListener(() => _isThatItPopUpPanel.Show());
+            _isThatItButton.gameObject.SetActive(false);
+
+            StartCoroutine(IsThatItYield());
+        }
+
+
+        IEnumerator IsThatItYield()
+        {
+            yield return new WaitForSeconds(25);
+
+            SoundManager.Play(GameManager.Data.Sounds.BlopA);
+            _isThatItButton.gameObject.SetActive(true);
+        }
     }
 }
